f3eab8b06ba90b9adb38f50640961c70e980ad5a
yhanhasan <46043692+yhan22esia@users.noreply.github.com>
2022-11-11 16:31:45 +0700

    ui: add splash screen

---
+++ app/src/main/java/com/example/liveattendanceapp/views/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.example.liveattendanceapp.views.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.liveattendanceapp.R
+import com.example.liveattendanceapp.R.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_splash)
+    }
+}
